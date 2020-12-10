d6962d08862ca9f9f085445a5a667c2643d6e8f3
Jacob <72887386+jacobpaul20c@users.noreply.github.com>
2020-12-11 00:57:42 +0530

    Update global_bans.py
    Some

--- cinderella/modules/global_bans.py
+++ cinderella/modules/global_bans.py
@@ -113,7 +113,7 @@ def gban(bot: Bot, update: Update, args: List[str]):
 
         return
     
-    message.reply_text("⚡️ *Snaps the Banhammer* ⚡️")
+    message.reply_text("⚡️ * പണി വരുന്നുണ്ട് അവറാച്ച!* ⚡️")
     
     start_time = time.time()
     datetime_fmt = "%H:%M - %d-%m-%Y"
