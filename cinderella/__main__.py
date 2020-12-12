6f5e4647d438fca877cfe040cb81d67386aaa2a9
piku-adhi <73607487+piku-adhi@users.noreply.github.com>
2020-12-12 15:28:42 +0530

    __main__.py
    Main

--- cinderella/__main__.py
+++ cinderella/__main__.py
@@ -183,7 +183,7 @@ def send_start(bot, update):
     keyboard += [[InlineKeyboardButton(text="MT OFFICIAL ",url="http://t.me/MT_Officials"),InlineKeyboardButton(text="SHRUTHI",url="http://t.me/Riskgirl_Bot")]]
     keyboard += [[InlineKeyboardButton(text="DEVELOPERS",url="http://t.me/mtoffbotdev"),InlineKeyboardButton(text="HONEY BEES",url="http://t.me/HoneyBeesChat")]]
     keyboard += [[InlineKeyboardButton(text="HELP",callback_data="help_back")]]
-    keyboard += [[InlineKeyboardButton(text="SOURCE",text="പോടാ മൈരെ")]]
+    keyboard += [[InlineKeyboardButton(text="SOURCE",url="http://t.me/nookkiirunnoippokittum")]]
 
     update.effective_message.reply_photo(img, PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_NAME, OWNER_ID), 
                                          reply_markup=InlineKeyboardMarkup(keyboard), disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
